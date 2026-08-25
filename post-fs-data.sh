82897780cb607ad4313b92513530407b1913e596
reDust Wildman <wizardwong9@gmail.com>
2026-08-25 06:50:22 -0700

    Synchronize early boot properties with APatch system.prop

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,8 +1,10 @@
 #!/system/bin/sh
 
 MODDIR=${0%/*}
-
 . "$MODDIR/common.sh"
 
 load_config
-apply_sim_country_props
+# Keep the next boot's APatch/Magisk early property file synchronized with config.conf.
+write_system_prop_file
+# Also enforce once during the current early boot. Telephony may overwrite later; service.sh watches that.
+apply_sim_country_props "post-fs-data"
