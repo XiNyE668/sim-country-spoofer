4fecd32ef98c473020f8b60791bc991e35eedb0c
reDust Wildman <wizardwong9@gmail.com>
2026-08-25 06:51:32 -0700

    Update installer messaging and APatch permissions

--- customize.sh
+++ customize.sh
@@ -1,10 +1,11 @@
 SKIPUNZIP=0
 
 ui_print " "
-ui_print "SIM Country Spoofer"
+ui_print "SIM Country Spoofer A16"
+ui_print "Optimized for APatch + Android 16 / LineageOS 23.2"
 ui_print "Default target: US / T-Mobile, MCCMNC 310260"
-ui_print "Open this module in KsuWebUI to change MCC/MNC/ISO from a visual interface."
-ui_print "You can also edit /data/adb/modules/sim_country_spoofer/config.conf manually."
+ui_print "WebUI: APatch module page -> Open WebUI"
+ui_print "Config: /data/adb/modules/sim_country_spoofer/config.conf"
 ui_print " "
 
 set_perm "$MODPATH" 0 0 0755
@@ -14,6 +15,9 @@ set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
 set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm "$MODPATH/webui.sh" 0 0 0755
 set_perm "$MODPATH/uninstall.sh" 0 0 0755
+if [ -f "$MODPATH/system.prop" ]; then
+  set_perm "$MODPATH/system.prop" 0 0 0644
+fi
 if [ -d "$MODPATH/webroot" ]; then
   set_perm_recursive "$MODPATH/webroot" 0 0 0755 0644
 fi
